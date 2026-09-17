f435bd121fe04387e6ac948ad94c265377b27dff
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-17 12:32:47 +0530

    added jwt verification on websocket endpoint

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,39 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 8000 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws , request) {
+  const url = request.url;
+  if(!url){
+    return;
+  }
+
+const queryParams = new URLSearchParams(url.split('?')[1]);
+const token = queryParams.get('token') || ""
+
+if (!JWT_SECRET) {
+  ws.close();
+  return;
+} 
+
+const decoded = jwt.verify(token , JWT_SECRET);
+
+if(typeof decoded == "string"){
+  ws.close();
+   return;
+
+}
+if (!decoded || !decoded.userId){
+
+  ws.close();
+  return;
+
+}
+
   ws.on("message", function message(data) {
     console.log("Received message:", data.toString());
     ws.send("pong");
   });
 });
-
-console.log("WebSocket server running on ws://localhost:8000");
