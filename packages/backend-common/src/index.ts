c9071b7ed3290a68c5a11620ad29ad9684bc04f7
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-21 09:34:04 +0530

    added frontend deploy url

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 export const JWT_SECRET = process.env.JWT_SECRET || "123123";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3002"
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://collab-sketch-docs-fh8m.vercel.app/"
 
